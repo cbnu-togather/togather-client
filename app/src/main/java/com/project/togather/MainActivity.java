b5fbf0437f88a2048dd27f76eac8d75b8b9dc1b3
developersung13 <developersung13@gmail.com>
2024-04-15 09:05:04 +0900

    [feat]: 로그인 액티비티 이동 로직 수정 (#5)

--- app/src/main/java/com/project/togather/MainActivity.java
+++ app/src/main/java/com/project/togather/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         binding.startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
