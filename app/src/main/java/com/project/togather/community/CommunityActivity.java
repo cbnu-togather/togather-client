3034020fe746456dfdd9155d37a20712f748ac6a
developersung13 <developersung13@gmail.com>
2024-04-21 13:49:32 +0900

    [feat]: '동' 설정 코드 추가 (#18-#21)

--- app/src/main/java/com/project/togather/community/CommunityActivity.java
+++ app/src/main/java/com/project/togather/community/CommunityActivity.java
@@ -68,8 +68,8 @@ public class CommunityActivity extends AppCompatActivity {
 
         // Adapter 안에 아이템의 정보 담기 (하드 코딩)
         postInfoItems.add(new PostInfoItem("https://gd.image-gmkt.com/%EC%BD%94%EC%B9%98-%EC%BD%94%EC%B9%98-%EB%B8%8C%EB%9D%BC%EC%9A%B4-%EA%B0%80%EC%A3%BD-%EB%82%A8%EB%85%80-%ED%95%99%EC%83%9D-%EC%A7%81%EC%9E%A5%EC%9D%B8-%EB%B0%B1%ED%8C%A9-%EC%B1%85%EA%B0%80%EB%B0%A9-%EC%A4%91%EA%B3%A0-%EB%AA%85%ED%92%88-%EA%B0%80%EB%B0%A9-%EC%9D%80%ED%94%BC%EC%95%84%EB%85%B8/li/600/574/2388574600.g_350-w-et-pj_g.jpg", "나눔", "가방 나눔해요~~", "새거입니다.", "개신동", 320, 1));
-        postInfoItems.add(new PostInfoItem("https://mblogthumb-phinf.pstatic.net/MjAyMjA2MDhfMjMy/MDAxNjU0NjgwMDEyNzQ2.jkfgtrFgZlbkEzgAXCmgEK7lzIcgJOwiMwNfjm9dfUog.weSpjt3puKOlolJv8fZUuNys5s7Vh9RAuJkX2Ikoe0cg.JPEG.dpfls111103/20220602%EF%BC%BF144812.jpg?type=w800", "고민/사연", "실외기 비둘기가 나뭇가지를..ㅠ", "실외기에 비둘기가 나뭇가지를..어제보니 실외기 위에 나뭇가지가 수북히 쌓여있더라구요...\n집은2층 입니다 실외기 커버 씌여놓은 상태구요..ㅠ\n혹시 경험 있으신 분이나 방법 아시는 분..\n조언 좀 부탁드릴게요ㅠ", "개신동", 320, 6));
-        postInfoItems.add(new PostInfoItem("", "맛집", "청주에 파스타 샐러드 / 샌드위치 맛집 있을까요?", "많은 추천 부탁드려요 ㅎㅎ", "개신동", 320, 2));
+        postInfoItems.add(new PostInfoItem("https://mblogthumb-phinf.pstatic.net/MjAyMjA2MDhfMjMy/MDAxNjU0NjgwMDEyNzQ2.jkfgtrFgZlbkEzgAXCmgEK7lzIcgJOwiMwNfjm9dfUog.weSpjt3puKOlolJv8fZUuNys5s7Vh9RAuJkX2Ikoe0cg.JPEG.dpfls111103/20220602%EF%BC%BF144812.jpg?type=w800", "고민/사연", "실외기 비둘기가 나뭇가지를..ㅠ", "실외기에 비둘기가 나뭇가지를..어제보니 실외기 위에 나뭇가지가 수북히 쌓여있더라구요...\n집은2층 입니다 실외기 커버 씌여놓은 상태구요..ㅠ\n혹시 경험 있으신 분이나 방법 아시는 분..\n조언 좀 부탁드릴게요ㅠ", "사창동", 320, 6));
+        postInfoItems.add(new PostInfoItem("", "맛집", "청주에 파스타 샐러드 / 샌드위치 맛집 있을까요?", "많은 추천 부탁드려요 ㅎㅎ", "율량동", 320, 2));
         postInfoItems.add(new PostInfoItem("", "이사/시공", "폐목재", "리모델링하면서 나온 다루끼 폐목재들을 어떻게 버려야 할까요..?", "개신동", 320, 0));
         postInfoItems.add(new PostInfoItem("", "부동산/주거", "전세계약 어떻게 하셨나요", "전세로 계약 시 계약서는 1대1로 쓰시나요?\n혹시 계약서만 부동산에서 작성할 수 있는 건가요?", "개신동", 320, 1));
         postInfoItems.add(new PostInfoItem("", "모임", "보드게임 모임사람 구합니다", "보드게임 모임 사람 모집합니다! 모임은 보통 주말에 충대 보드카페나 게임들고 일반카페도 가끔 갑니당☺️☺️ 게임 아는 거 없어도 가능해요! 루미큐브 할리갈리만 해 분신 분 가능! 티츄 좋아하거나 전략 게임 좋아하시는 분! 다양한 게임 배우고 싶으신 분! 환영합니다! 와서 같이 놀아요! 신천지 여미새 남미새 사절😒", "개신동", 320, 2));
@@ -154,8 +154,7 @@ public class CommunityActivity extends AppCompatActivity {
             TextView likedCnt_textView = convertView.findViewById(R.id.likedCnt_textView);
 
             postTitle_textView.setText(postInfoItem.getTitle());
-            Log.d("테스트", "" + postInfoItem.getDistrict());
-//            district_textView.setText(postInfoItem.getDistrict());
+            district_textView.setText(postInfoItem.getDistrict());
 
             long elapsedTime = postInfoItem.getElapsedTime();
             String elapsedTime_str;
