b6b48cc8c9ccf4dfab4171fd4cdae630abc3f1f0
김연신 <127587330+YeonShin@users.noreply.github.com>
2024-06-01 22:35:04 +0900

    [feat]: 모집글 삭제 기능 추가

--- app/src/main/java/com/project/togather/retrofit/interfaceAPI/RecruitmentAPI.java
+++ app/src/main/java/com/project/togather/retrofit/interfaceAPI/RecruitmentAPI.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
@@ -50,4 +51,7 @@ public interface RecruitmentAPI {
 
     @POST("groupbuy/like")
     Call<ResponseBody> setRecruitmentPostLike(@Query("groupBuyId") int postId);
+    @DELETE("groupbuy/{groupBuyId}")
+    Call<ResponseBody> deleteRecruitmentPost(@Path("groupBuyId") int postId);
+
 }
