7e9dafce2e61d9ebe683312ceeba1539440a8c1a
김연신 <127587330+YeonShin@users.noreply.github.com>
2024-06-08 23:00:31 +0900

    [feat]: 채팅 목록 연속적으로 조회 및 채팅방 나가기 기능 추가

--- app/src/main/java/com/project/togather/retrofit/interfaceAPI/ChatAPI.java
+++ app/src/main/java/com/project/togather/retrofit/interfaceAPI/ChatAPI.java
@@ -21,6 +21,8 @@ public interface ChatAPI {
     Call<ResponseBody> raiseHand(@Path("groupBuyId") int postId);
     @PUT("acceptWaiting/{waitingId}")
     Call<ResponseBody> acceptWaiting(@Path("waitingId") int waitingId);
+    @DELETE("declineWaiting/{waitingId}")
+    Call<ResponseBody> deleteNotification(@Path("waitingId") int waitingId);
 
     @GET("notifications")
     Call<List<NotificationInfoItem>> getNotificationList();
@@ -30,6 +32,7 @@ public interface ChatAPI {
     Call<List<ChatDetailInfoItem>> getChatRoomDetails(@Path("chatRoomId") int chatRoomId);
     @PUT("chatrooms/{chatRoomId}/messages")
     Call<ResponseBody> sendMessage(@Path("chatRoomId") int chatRoomId, @Body ChatMessageRequest chatMessageRequest);
-    @DELETE("declineWaiting/{waitingId}")
-    Call<ResponseBody> deleteNotification(@Path("waitingId") int waitingId);
+    @DELETE("chatrooms/{chatRoomId}/leave")
+    Call<ResponseBody> leaveChatRoom(@Path("chatRoomId") int chatRoomId);
+
 }
